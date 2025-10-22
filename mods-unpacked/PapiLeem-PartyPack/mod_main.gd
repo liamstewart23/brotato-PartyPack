b6e0711c5e073b67f4d8374e743361d0c70ce528
Liam Stewart <15363506+liamstewart23@users.noreply.github.com>
2025-10-21 20:49:49 -0400

    Initial Mod setup + Lucky Sock item

---
+++ mods-unpacked/PapiLeem-PartyPack/mod_main.gd
@@ -0,0 +1,53 @@
+extends Node
+
+const MOD_DIR = "PapiLeem-PartyPack"
+
+var mod_dir_path := ""
+var item_extensions_dir_path := ""
+var weapon_extensions_dir_path := ""
+
+var item_files = []
+var weapon_files = []
+
+func _init():
+	mod_dir_path = ModLoaderMod.get_unpacked_dir().plus_file(MOD_DIR)
+	item_extensions_dir_path = mod_dir_path.plus_file("extensions/items")
+	#weapon_extensions_dir_path = mod_dir_path.plus_file("extensions/weapons")
+
+func _ready() -> void:
+	var ContentLoader = get_node("/root/ModLoader/Darkly77-ContentLoader/ContentLoader")
+	
+	# Load items
+	item_files = get_files_in_subdirs(item_extensions_dir_path)
+	for item_path in item_files:
+		ContentLoader.load_data(item_path, "PapiLeem")
+	
+	# Load weapons
+	#weapon_files = get_files_in_subdirs(weapon_extensions_dir_path)
+	#for weapon_path in weapon_files:
+#		ContentLoader.load_data(weapon_path, MOD_DIR)
+
+
+# Helper function to get the .tres file from each subdirectory
+func get_files_in_subdirs(base_dir_path: String) -> Array:
+	var files = []
+	var dir = Directory.new()
+
+	if dir.open(base_dir_path) != OK:
+		print("Cannot open directory: ", base_dir_path)
+		return files
+
+	dir.list_dir_begin()
+	var name = dir.get_next()
+	while name != "":
+		if dir.current_is_dir() and name != "." and name != "..":
+			var path = base_dir_path.plus_file(name).plus_file(name + "_item.tres")
+			if dir.file_exists(path):
+				files.append(path)
+			else:
+				print("No .tres file for ", name, " at ", path)
+		name = dir.get_next()
+	dir.list_dir_end()
+
+	return files
+
